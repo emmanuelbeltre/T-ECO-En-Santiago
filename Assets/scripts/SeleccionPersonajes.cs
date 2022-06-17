4be94fab222b5251eb5192f88695e677f8f03d79
emmanuelbeltre <beltrelgg2@gmail.com>
2022-06-17 10:28:26 -0400

    Se agregó el escript SeleccionPersonajes para selección, deselección de personajes y mensaje de alerta indicando cuando se intenta seleccionar un 5 personaje

---
+++ Assets/scripts/SeleccionPersonajes.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class SeleccionPersonajes : MonoBehaviour{
+	
+	//Estas variables representan cada botón de selección de personajes
+	
+	[Header("Botones/Selección")]	
+	public Button botonPersonaje1;
+	public Button botonPersonaje2;
+	public Button botonPersonaje3;
+	public Button botonPersonaje4;
+	public Button botonPersonaje5;
+	public Button botonPersonaje6;
+	
+   
+	//Estas variables representan los videos de cada personaje que se puede elejir.
+	[Header("Personajes")]
+	public GameObject PerspnajeVideo1;
+	public GameObject PerspnajeVideo2;
+	public GameObject PerspnajeVideo3;
+	public GameObject PerspnajeVideo4;
+	public GameObject PerspnajeVideo5;
+	public GameObject PerspnajeVideo6;
+	
+	
+	
+	[Header ("Variables extras")]
+	public int	 conteo;
+	public Text mensajeAlerta;
+	public GameObject[] Personas;
+
+    void Start()
+    {
+	    conteo = 0;
+ 
+	    botonPersonaje1.onClick.AddListener(()=> MostrarPersonajePantalla(PerspnajeVideo1));
+	    botonPersonaje2.onClick.AddListener(()=> MostrarPersonajePantalla(PerspnajeVideo2));
+	    botonPersonaje3.onClick.AddListener(()=> MostrarPersonajePantalla(PerspnajeVideo3));
+	    botonPersonaje4.onClick.AddListener(()=> MostrarPersonajePantalla(PerspnajeVideo4));
+	    botonPersonaje5.onClick.AddListener(()=> MostrarPersonajePantalla(PerspnajeVideo5));
+	    botonPersonaje6.onClick.AddListener(()=> MostrarPersonajePantalla(PerspnajeVideo6));
+
+		
+	    //sizeArreglo.onClick.AddListener(()=> SizeDelArreglo(Personas, "Persona"));
+	    //sizeArreglo.onClick.AddListener(()=> MostrarAlerta());
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    
+	//Con esta función podremos mostrar y ocultar cada uno de los personaje con solo presionando su botón correspondiente
+	void MostrarPersonajePantalla(GameObject personaje){
+	
+		if (personaje.activeSelf)
+		{
+			personaje.SetActive(false);
+			conteo = conteo-1; 
+			Debug.Log(conteo);
+		}else{
+			conteo = conteo+1;
+			//Con esta condición al al tener 4 personajes seleccionados no se nos permetirá seleccionar un quinto personaje.
+			if(conteo>4){
+				MostrarAlerta();
+				conteo = conteo-1;
+				Debug.Log(conteo);
+				
+				return ;
+			}
+			
+			personaje.SetActive(true);
+			Debug.Log(conteo);
+		}
+	
+		
+	}
+	//Esta función ejecuta mi IEnumerator, que basicamente implementa dos métodos
+	void MostrarAlerta()=>StartCoroutine(MiContador());
+
+	//Este método me permite mostrar el mensaje en pantalla durante solo 3 segundos y luego sustituir el texto por una cadena vacía
+	IEnumerator MiContador()
+	{
+		mensajeAlerta.text = "Solo Debe Seleccionar 4 personajes, intente deseleccionar uno de los ya seleccionado";
+		yield return new WaitForSeconds(3);
+		mensajeAlerta.text = "";
+	 
+	}
+	
+	void SizeDelArreglo(GameObject[] miArr, string miTag){
+		miArr = GameObject.FindGameObjectsWithTag(miTag);
+		
+		
+		for(int i =0; i <= miArr.Length-1 ; i++){
+			Debug.Log("El objeto "+  i + " tiene el nombre" + miArr[i].name);
+		}
+	}
+    
+    
+}
